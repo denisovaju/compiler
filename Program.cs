5c5e48aa2f4cb17020eb3d075b3de160173f2b1b
Julia Denisova <exdidyma@gmail.com>
2016-08-26 02:17:49 +0800

    1. Rewrote the function in class Parser

--- Program.cs
+++ Program.cs
@@ -11,6 +11,9 @@ namespace ConsoleApplication1
 
     class LexicalAnalysis
     {
+        public const string INTEGER = "INTEGER";
+        public const string PLUS = "PLUS";
+
         private string characters;
         private string lexema = "0123456789+";
 
@@ -63,7 +66,6 @@ namespace ConsoleApplication1
     //написать конструктор
     class ArithmeticAnalysis : LexicalAnalysis
     {
-        private static List<string> null_l = new List<string> { "No correct input\n" };
         private List<string> token = new List<string>();
 
         //демонстрация
@@ -75,7 +77,7 @@ namespace ConsoleApplication1
         //возвращает токены, только для однозначных чисел, порядок следования числа и оператора не учитывается
         public List<string> get_token(string s)
         {
-            string temp;
+            string[] temp = new string[2];
             int counter = 0;
             for (int i = 0; i < s.Length; i++)
             {
@@ -84,8 +86,10 @@ namespace ConsoleApplication1
                     counter += 1;
                     if (counter == 1)
                     {
-                        temp = "operator" + " : " + s[i];
-                        token.Add(temp);
+                        //temp = "operator" + " : " + s[i];
+                        temp[0] = PLUS;
+                        temp[1] = s[i] + "#";
+                        token.AddRange(temp);
                         continue;
                     }
                     else
@@ -104,26 +108,31 @@ namespace ConsoleApplication1
                     {
                         if (counter == 1)
                         {
-                            temp = "left" + " : " + s[j];
-                            token.Add(temp);
+
+                            temp[0] = INTEGER;
+                            temp[1] = s[j] + "#";
+                            token.AddRange(temp);
                             counter += 1;
                             break;
                         }
                         else if (counter == 2)
                         {
-                            temp = "right" + " : " + s[j];
-                            token.Add(temp);
+                            temp[0] = INTEGER;
+                            temp[1] = s[j] + "#";
+                            token.AddRange(temp);
                             counter += 1;
                             break;
                         }
                     }
                 }
+                return token;
             }
             else
             {
-                return null_l;
+                token.Clear();
+                token.Add("No correct input");
+                return token;
             }
-            return token;
         }
 
         //просто выводит список с токенами
@@ -137,44 +146,52 @@ namespace ConsoleApplication1
     }
 
     class Parser
-    {
+    {        
+        //опять для однозначных чисел
         public void compiler(List<string> l)
         {
-            int summa = 0;
-            string oper = l[0];
-            string left = l[1];
-            string right = l[2];
-            string o = "0";
-            string le = "0";
-            string r = "0";
-            for (int i = 0; i < oper.Length; i++)
+            int a = 0;
+            int b = 0;
+            string operation = "0";
+            int counter = 0;
+
+            for (int i = 0; i < l.Count; i++)
             {
-                if (i == 7)
+                //Console.WriteLine(l[i]);
+                if (i % 2 != 0)
                 {
-                    le = left.Substring(i, 1);
-                }
-                else if (i == 8)
-                {
-                    r = right.Substring(i, 1);
-                }
-                else if(i == 11)
-                {
-                    o = oper.Substring(i, 1);
+                    for (int j = 0; j < l.Count; j++)
+                    {
+                        if (l[j].EndsWith("#"))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            if (counter == 0)
+                            {
+                                operation = l[i].Substring(0,1);
+                                counter += 1;
+                            }
+                            else if (counter == 1)
+                            {
+                                a = Convert.ToInt32(l[i].Substring(0,1));
+                                counter += 1;
+                            }
+                            else if (counter == 2)
+                            {
+                                b = Convert.ToInt32(l[i].Substring(0,1));                           
+                            }
+                        }
+                    }
                 }
             }
 
-            if (o == "+")
+            if (operation == "+")
             {
-                int a = 0;
-                int b = 0;
-                a = Convert.ToInt32(le);
-                b = Convert.ToInt32(r);
-                summa = a + b;
-                
-            }
-            
-            Console.WriteLine(summa);
-            
+                int summa = a + b;
+                Console.WriteLine(summa);
+            }            
         }
     }
 
@@ -191,7 +208,7 @@ namespace ConsoleApplication1
 
 
                 ArithmeticAnalysis number = new ArithmeticAnalysis();
-                number.demo(lexema.Characters);
+                //number.demo(lexema.Characters);
 
                 List<string> token = new List<string>();
                 token = number.get_token(lexema.Characters);
